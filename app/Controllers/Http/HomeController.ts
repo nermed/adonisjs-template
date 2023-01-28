372edb5dc946189047ce0b2a9d0ad36ccf180ad5
donasyst <cestpasgrave2020@gmail.com>
2023-01-28 13:12:04 +0200

    first commit

---
+++ app/Controllers/Http/HomeController.ts
@@ -0,0 +1,11 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class HomeController {
+    async index({ view }: HttpContextContract) {
+        return view.render("pages/home/index", {title: "Home"})
+    }
+
+    async settings({ view }: HttpContextContract) {
+        return view.render("pages/home/settings", { title: "Settings" })
+    }
+}
