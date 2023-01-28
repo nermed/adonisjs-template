2d4af4c5b485fc47e1c307820a8f04e16db957a1
donasyst <cestpasgrave2020@gmail.com>
2023-01-28 13:15:06 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+// app entrypoint
+// import 'jquery';
+import "moment";
+import "../views/layout/dist/js/adminlte.js";
+import "../views/layout/dist/js/demo.js";
+import "../views/layout/dist/js/pages/dashboard.js";
+import "../views/layout/plugins/jquery/jquery.min.js";
+import "../views/layout/plugins/bootstrap/js/bootstrap.bundle.min.js";
+import "../views/layout/plugins/chart.js/Chart.min.js";
+import "../views/layout/plugins/sparklines/sparkline.js";
+import "../views/layout/plugins/jqvmap/jquery.vmap.min.js";
+import "../views/layout/plugins/jqvmap/maps/jquery.vmap.usa.js";
+import "../views/layout/plugins/jquery-knob/jquery.knob.min.js";
+// import "../views/layout/plugins/moment/moment.min.js"
+import "../views/layout/plugins/daterangepicker/daterangepicker.js";
+import "../views/layout/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js";
+// import 'summernote';
+// import "../views/layout/plugins/summernote/summernote-bs4.min.js"
+import "../views/layout/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js";
+
+
+
+$.widget.bridge("uibutton", $.ui.button);
